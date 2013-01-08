b3d739302f48ff5cbb96b9df0f7dcc2f88c0619f
jduque <jduque@activomultimedia.com>
2013-01-08 10:35:43 -0500

    08/01/2013

--- index(page-with-comments).php
+++ index(page-with-comments).php
@@ -6,8 +6,8 @@ header('Access-Control-Allow-Origin: *');
  */
   $cont = 0;
   if($_GET['ajax']==true){
-    global $more;    
-    $more = 0;       
+    global $more;    // Declare global $more (before the loop).
+    $more = 0;       // Set (inside the loop) to display content above the more tag.
     if ( have_posts() ) : 
       while ( have_posts() ) : 
         the_post();
@@ -54,17 +54,86 @@ header('Access-Control-Allow-Origin: *');
               <h1 class="section-heading"><?php _e( 'Main menu', 'themename' ); ?></h1>
               <div class="skip-link visuallyhidden"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'themename' ); ?>"><?php _e( 'Skip to content', 'themename' ); ?></a></div>
               <?php wp_nav_menu( array('menu' => 'Top')); ?>
-            </nav>
-        </header>
+            </nav><!-- #access -->
+        </header><!-- #branding -->
         <div id="content">
           <?php get_template_part( 'loop', 'index' ); ?>
-        </div>
+        </div> <!-- #content -->
         <div class="next_slide"><p>next</p></div>
+      </div> <!-- #primary -->
+    </script>
+
+    <!-- *************** INDEX ******************************************************** -->
+    <script type="text/html" id="index-template">
+      <div class="index-container">
+        <?php wp_nav_menu( array ("container" => false) ); ?>
+        <div class="gallery_header menu_header_index">
+          
+          <h4 class="title"></h4>
+        </div>
+        /*<center>*/
+          <ul class="main-nav span3">
+            <li class="gallery_title">Galería</li>
+            <?php
+                wp_list_categories('hide_empty=0&child_of=7');
+            ?>
+          </ul>
+        /*</center>*/
+        <div class="first_image">
+          <%
+            cont=0;
+            _.each(imgs.at(0).attributes, function(thisGallery){ %>
+              <img src="<%= thisGallery.images[0].image %>" class="<%= thisGallery.idAttribute %>" />
+              <div class="<%= thisGallery.idAttribute %>">
+                <%= thisGallery.description %>
+              </div>
+            <%  
+            })
+          %>
+        </div>
+      </div>
+      <div id="log"></div> 
+      <div id="loading"></div>
+    </script>
+
+    <!-- *************** CONTACT ******************************************************** -->
+    <script type="text/html" id="contact-template">
+    </script>
+
+    <!-- *************** ABOUT ********************************************************** -->
+    <script type="text/html" id="about-template">
+      <nav class="page_nav">You are here - <%= get("nombre") %></nav>
+      <div class="about_container">
+        <div class="content span12">
+          <div class ="title span9">
+            <h1><%= get("title") %></h1>
+          </div>
+          <ul class="outer_content">
+            <li class="parent span5"><p><%= get("leftParagraph") %></p></li>
+            <li class="parent span2">
+              <h3>awards</h3>
+              <ul>
+                <li><%= get("awards") %></li>
+              </ul>
+            </li>
+            <li class="parent span2">
+              <h3>recognize</h3>
+                <ul>
+                  <li><%= get("recognize") %></li>
+                </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </script>
 
     <!-- *************** PAGE ******************************************************* -->
-    <script type="text/html" id="page-template">
+    <script type="text/html" id="page-template">        
+        <!-- ******************* -->
+        <!-- *** please note *** --> 
+        <!-- ******************* -->        
+        <!--<% pageModel.at(0).attributes; %> // Call all current JSon data -->
+        <!--<% pageModel.at(0).attributes.customName; %> //example custom, where customName is the name of the custom field -->
     <div class="page_container">
       <div class="prev_slide"><p>prev</p></div>
         <%
@@ -77,6 +146,7 @@ header('Access-Control-Allow-Origin: *');
                 <div class="page_content">
                   <%= pageModel.at(0).attributes.content %>
                 </div>
+                <!-- BEGIN show all custom fields -->
                 <% 
                   customField = pageModel.at(0).attributes.custom;
                   cont=0;
@@ -85,7 +155,8 @@ header('Access-Control-Allow-Origin: *');
                       <div class="custom <%= customName %>"><%= customField[customName] %></div>
                     <% }
                     cont++;
-                  } %>
+                  } %> <!-- END for -->
+                <!-- END show all custom fields -->
               </div>
             </div>
           <%  
@@ -99,7 +170,7 @@ header('Access-Control-Allow-Origin: *');
                 <ul class="nav_ul">
                 <% _.each(thisPages.pages, function(page){ %>
                     <li><a href="#<%= page.subpageId %>" class="<%= page.slug %>"><%= page.title %></a></li>
-                <% });  %>
+                <% });  %>  <!-- END _.each -->
                 </ul>
                 <nav class="bottom_arrow"><a href="" class="">bottom</a></nav>
               </div>
@@ -109,6 +180,7 @@ header('Access-Control-Allow-Origin: *');
                 <div class="inner_content <%= thisPage.slug %>">
                   <h2 class="subpage_title"><%= thisPage.title %></h2>
                   <div class="content_subpage"><%= thisPage.content %></div>
+                  <!-- BEGIN show all custom fields -->
                   <% 
                     customField = thisPage.custom;
                     cont=0;
@@ -117,17 +189,23 @@ header('Access-Control-Allow-Origin: *');
                         <div class="custom <%= customName %>"><%= customField[customName] %></div>                 
                       <% }
                       cont++;
-                    } %> 
+                    } %> <!-- END for -->
+                  <!-- END show all custom fields -->
                 </div>
               </div>
-            <% });  %> 
-        <% } %>
+            <% });  %> <!-- END _.each -->
+        <% } %> <!-- END else -->
         <div class="next_slide"><p>next</p></div>
       </div>
     </script>
 
     <!-- *************** SINGLE ******************************************************* -->
-    <script type="text/html" id="single-template">  
+    <script type="text/html" id="single-template">        
+        <!-- ******************* -->
+        <!-- *** please note *** --> 
+        <!-- ******************* -->        
+        <!--<% pageModel.at(0).attributes; %> // Call all current JSon data -->
+        <!--<% pageModel.at(0).attributes.customName; %> //example custom, where customName is the name of the custom field -->
         <% $("body").attr("class",pageModel.at(0).attributes.bodyClass.toString().replace(/,/g," ")); %>
       <div class="single_container">
         <%
@@ -139,6 +217,7 @@ header('Access-Control-Allow-Origin: *');
                 <div class="page_content">
                   <%= pageModel.at(0).attributes.content %>
                 </div>
+                <!-- BEGIN show all custom fields -->
                 <% 
                   customField = pageModel.at(0).attributes.custom;
                   cont=0;
@@ -147,14 +226,19 @@ header('Access-Control-Allow-Origin: *');
                       <div class="custom <%= customName %>"><%= customField[customName] %></div>
                     <% }
                     cont++;
-                  } %>
+                  } %> <!-- END for -->
+                <!-- END show all custom fields -->
               </div>
             </div>
       </div>
     </script>
 
     <!-- *************** SEARCH ******************************************************* -->
-    <script type="text/html" id="search-template">  
+    <script type="text/html" id="search-template">        
+        <!-- ******************* -->
+        <!-- *** please note *** --> 
+        <!-- ******************* -->        
+        <!--<% searchModel.at(0).attributes[0]; %> // Call all current JSon data -->
       <div class="single_container">
         <%
           thisSearch = searchModel.at(0).attributes[0];
@@ -174,7 +258,11 @@ header('Access-Control-Allow-Origin: *');
 
 
     <!-- ***************  ARCHIVE ******************************************************* -->
-    <script type="text/html" id="archive-template"> 
+    <script type="text/html" id="archive-template">        
+        <!-- ******************* -->
+        <!-- *** please note *** --> 
+        <!-- ******************* -->        
+        <!--<% archiveModel.at(0).attributes[0]; %> // Call all current JSon data -->
       <div class="single_container">
         <%
           thisArchive = archiveModel.at(0).attributes[0];
